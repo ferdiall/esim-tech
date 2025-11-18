6231bebbf28a8795e15c005f3574b6fb165ec333
ferdiall <frdal2847@gmail.com>
2025-11-18 18:01:31 +0100

    Fix static root and add logo

--- esim_site/settings.py
+++ esim_site/settings.py
@@ -128,10 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "esim_site" / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
